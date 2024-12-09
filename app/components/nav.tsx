9cf13da3c27bf4360616b07b5599ea9cb895fb8c
Gheorghe Botnaru <94964382+shaquill-oatmeal@users.noreply.github.com>
2024-12-09 16:16:27 +0100

    feat: add blogs

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -4,9 +4,9 @@ const navItems = {
   "/": {
     name: "home",
   },
-  // '/blog': {
-  //   name: 'blog',
-  // },
+  "/blog": {
+    name: "blog",
+  },
   // 'https://vercel.com/templates/next.js/portfolio-starter-kit': {
   //   name: 'deploy',
   // },
