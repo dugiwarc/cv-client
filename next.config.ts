c2644737a7dc52f03777a804f970a7c88870da82
Gheorghe Botnaru <94964382+shaquill-oatmeal@users.noreply.github.com>
2024-12-09 11:35:45 +0100

    feat: add config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // This enables static HTML export
+  distDir: "out",
 };
 
 export default nextConfig;
